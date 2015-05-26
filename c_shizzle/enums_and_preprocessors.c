7b5fe8d0dd43da60f1d22139ab20b3f3336b9b19
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2015-05-26 18:40:39 +0530

    more

---
+++ c_shizzle/enums_and_preprocessors.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  enum types {
+    ZERO, ONE, TWO, THREE
+  } types;
+
+  printf("first :: %d\n", THREE);
+
+  //  enum dytes {
+  //ZERO, ONE, TWO, THREE
+  //} dytes;
+
+  //printf("second :: %d", dytes.ZERO);
+  
+}
+
