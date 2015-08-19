d61a48d2f40c9eec837483c0ba8225809f7cea73
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2015-08-19 15:39:45 +0530

    Python tic tac toe
    made more of python tic tac toe
    
    some changes

--- BE/daa/pytic_tac_toe/gui.py
+++ BE/daa/pytic_tac_toe/gui.py
@@ -1,27 +1,55 @@
 from Tkinter import *
 from Tkconstants import *
 import tkMessageBox
+import logic
+
+class MiniMax(object):
+  def __init__(self):
+    pass
+
+  def compute(self, pressed_button_name, button_list):
+    pass
 
 class GUI(object):
   """GUI"""
 
   def __init__(self):
     self.app = Tk()
-    labelframe = LabelFrame(self.app, text="Lets play tic tac toe")
-    labelframe.pack(fill="both", expand="yes")
+    self.minimax = MiniMax()
+    buttonframe = LabelFrame(self.app)
+    buttonframe.grid(row=0)
 
-    left = Label(labelframe, text="Inside the LabelFrame")
-    left.pack()
+    tagframe = LabelFrame(self.app)
+    tagframe.grid(row=1)
+    tag = Label(tagframe, text="Inside the buttonframe")
 
-    def action_for_x(event):
-      name = event.widget
-      name1 = name._name
-      event.widget["text"] = str(name1)
+    self.button_list = []
+    for x in xrange(0,9):
+      button = Button(buttonframe,text="",name=str(x),height="10",width="10")
+      button.bind('<Button-1>', self._report_change_to_minimax)
+      self.button_list.append(button)
 
-    button = Button(labelframe,text="",name="b1HELLOE")
-    button.pack()
-    button.bind('<Button-1>',action_for_x)
+    k = 0
+    for i in xrange(0,3):
+      for j in xrange(0,3):
+        self.button_list[k].grid(row=i, column=j)
+        k += 1
 
     self.app.mainloop()
 
+  def _check_for_already_set(self, button):
+    pass
+
+  def _update_buttons(self, solution_from_bot):
+    pass
+
+  def _report_change_to_minimax(self,event):
+    button = event.widget
+    self._check_for_already_set(button)
+    button["text"] = "X"
+    solution_from_bot = self.minimax.compute(button._name, self.button_list)
+    self._update_buttons(solution_from_bot)
+
 x = GUI()
+
+# www.ayatists.in
