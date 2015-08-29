a2f263398a99396bee53b0525306d1d11ec93060
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2015-08-30 03:21:57 +0530

    more to tic tac tor

--- BE/daa/pytic_tac_toe/gui.py
+++ BE/daa/pytic_tac_toe/gui.py
@@ -9,69 +9,148 @@ GRID_SIZE = 3
 
 class Grid(object):
   def __init__(self, player_state, **kwargs):
-    if kwargs['without_buttons'] == True:
+    if kwargs.has_key('without_buttons') and kwargs['without_buttons'] == True:
       self.grid = player_state
     else:
       self.grid = []
-      for i in xrange(0,3):
-        grid.append([])
-        for j in xrange(0,3):
-          grid[i].append(player_state[i*GRID_SIZE + j]["text"])
+      for i in xrange(0,GRID_SIZE):
+        self.grid.append([])
+        for j in xrange(0,GRID_SIZE):
+          self.grid[i].append(player_state[i*GRID_SIZE + j]["text"])
+
+    print self.grid
 
   @classmethod
   def without_buttons(cls, buttons):
     Grid(buttons, without_buttons=True)
 
-  def is_over():
-    for row in xrange(0,3):
-      for col in xrange(0,3):
-        if self.grid[row][col] != "":
+  def is_over(self):
+    # Check if O or X win
+    if self.o_wins() or self.x_wins():
+      return True
+    # Check for all boxes full
+    for row in xrange(0,GRID_SIZE):
+      for col in xrange(0,GRID_SIZE):
+        if self.grid[row][col] == "":
           return False
 
     return True
 
-  def get_all_possible_states():
+  def check_win(self, sym):
+
+    # Check rows
+    for row in xrange(0,GRID_SIZE):
+      we_have_a_winner = True
+
+      for col in xrange(0,GRID_SIZE):
+        if self.grid[row][col] != sym:
+          we_have_a_winner = False
+
+      if we_have_a_winner:
+        return True
+      else:
+        return False
+
+    # Check cols
+    for col in xrange(0,GRID_SIZE):
+      we_have_a_winner = True
+
+      for row in xrange(0,GRID_SIZE):
+        if self.grid[row][col] != sym:
+          we_have_a_winner = False
+
+      if we_have_a_winner:
+        return True
+      else:
+        return False
+
+    we_have_a_winner = True
+    # Check one diag
+    for diag in xrange(0,GRID_SIZE):
+      if self.grid[diag][diag] != sym:
+        we_have_a_winner = False
+
+    if we_have_a_winner:
+      return True
+    else:
+      return False
+
+    # Check the other diag
+    if self.grid[2][0] == self.grid[1][1] == self.grid[0][2] == sym:
+      return True
+    else:
+      return False
+
+  def o_wins(self):
+    self.check_win('O')
+
+  def x_wins(self):
+    self.check_win('X')
+
+  def get_all_possible_states(self, move_symbol):
     possible_states = []
 
-    for i in xrange(0,3):
-      for j in xrange(0,3):
+    for i in xrange(0, GRID_SIZE):
+      for j in xrange(0, GRID_SIZE):
         if self.grid[i][j] == "":
-          temp = copy.copy(self.grid)
-          temp[i][j] = "O"
-          possible_states.append(Grid.without_buttons(temp))
-
+          temp = copy.deepcopy(self.grid)
+          temp[i][j] = move_symbol
+          possible_states.append(Grid(temp,without_buttons=True))
+    
     return possible_states
+
+  def __str__(self):
+    return ' '.join([str(item) for item in self.grid])
+
+  def to_list(self):
+    return self.grid
 
 class MiniMax(object):
   def __init__(self):
     pass
 
-  def compute(self, pressed_button_name, button_list):
+  def compute(self, button_list):
     game = Grid(button_list)
+    recent_move = 'X'
+    self._actual_compute(game, recent_move,0)
 
-    _actual_compute(game)
+    return self.chosen_grid
 
-  def _score(self, game):
-    arr = []
-    # check horizontal winning combinations
-    for row in xrange(0,GRID_SIZE):
-      for col in xrange(0,GRID_SIZE):
-        arr.append(self.grid[row][col])
+  def _score(self, game, depth):
+    if game.o_wins():
+      return 10 - depth
+    elif game.x_wins():
+      return depth - 10
+    else:
+      return 0
 
-      if len(set(arr)) == 1:
-        return True
-
-  def _actual_compute(self, game):
+  def _actual_compute(self, game, recent_move,depth):
     if game.is_over():
-      return _score(game)
+      return self._score(game, depth)
 
+    depth += 1
     scores = []
-    moves = []
+    moves  = []
 
-    all_possible_states = game.get_all_possible_states()
+    next_move = ('O' if recent_move == 'X' else 'X')
+    all_possible_states = game.get_all_possible_states(next_move)
+
+    print "\nstates\n" + str(all_possible_states)
     for state in all_possible_states:
-      _actual_compute(state)   
+      scores.append(self._actual_compute(state, next_move, depth))
+      moves.append(state)
 
+    print "scores list :: " + str(scores)
+    if recent_move == 'O':
+      # Computer move so maximize scores.
+      max_score_index = scores.index(max(scores))
+      self.chosen_grid = moves[max_score_index]
+      return scores[max_score_index]
+    else:
+      # Human move so minimize scores.
+      min_score_index = scores.index(min(scores))
+      self.chosen_grid = moves[min_score_index]
+      return scores[min_score_index]
 
 class GUI(object):
   """GUI"""
@@ -104,13 +183,19 @@ class GUI(object):
     pass
 
   def _update_buttons(self, solution_from_bot):
-    pass
+    lst = solution_from_bot.to_list()
+
+    k = 0
+    for row in xrange(0,GRID_SIZE):
+      for col in xrange(0,GRID_SIZE):
+        self.button_list[k]['text'] = lst[row][col]
+        k += 1
 
   def _report_change_to_minimax(self,event):
     button = event.widget
     self._check_for_already_set(button)
     button["text"] = "X"
-    solution_from_bot = self.minimax.compute(button._name, self.button_list)
+    solution_from_bot = self.minimax.compute(self.button_list)
     self._update_buttons(solution_from_bot)
 
 x = GUI()
