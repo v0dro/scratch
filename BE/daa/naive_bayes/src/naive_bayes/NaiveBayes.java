d828119fc38b9a978b865ab7afd3e83f933c1d19
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2015-09-01 18:07:18 +0530

    started with naive bayes

---
+++ BE/daa/naive_bayes/src/naive_bayes/NaiveBayes.java
@@ -0,0 +1,12 @@
+package naive_bayes;
+import java.util.Vector;
+
+public class NaiveBayes {
+  public void fit(Vector<Data> database, String[] names) {
+    
+  }
+  
+  public Data predict(Data predictor, String name) {
+    
+  }
+}
---
+++ BE/daa/naive_bayes/src/naive_bayes/Runner.java
@@ -0,0 +1,9 @@
+package naive_bayes;
+
+import java.util.Vector;
+
+public class Runner {
+  public static void main(String[] args) {
+    new Starter();
+  }
+}
