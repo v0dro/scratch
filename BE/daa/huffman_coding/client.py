b4d4135deedc7d2de187aa586644a9ce616d7657
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2015-09-24 15:44:08 +0530

    done with huffman

--- BE/daa/huffman_coding/client.py
+++ BE/daa/huffman_coding/client.py
@@ -19,25 +19,18 @@ def compute_freq_table(freqs):
 
   return dict_list
 
-def create_huffman_tree(freq_table):
-  return huffman.HuffmanTree(freq_table)
-
-def generate_huffman_codes(huffman_tree):
-  return huffman_tree.traverse()
-
 def pretty_print_code_table():
   print "============ HUFFMAN CODES ============"
   print "CHAR FREQ CODE"
   for tup in reversed(sorted(freq_table, key=lambda x: x[1])):
     print tup[0] + "     " + str(tup[1]) + "   " + encoding[tup[0]]
 
-
 string = "I have been and always shall be your friend"
 print "The string is : " + string
 
 freq_table = compute_freq_table(calculate_frequencies())
-tree       = create_huffman_tree(freq_table)
-encoding   = generate_huffman_codes(tree)
+tree       = huffman.HuffmanTree(freq_table)
+encoding   = tree.traverse()
 pretty_print_code_table()
 
 encoded = ""
@@ -52,7 +45,39 @@ print "\nConnecting to server at localhost on port 2345..."
 s = socket.socket()
 s.connect(('localhost', 2345))
 
-print "Sending Huffman tree over socket..."
+print "Sending Huffman tree and encoded string over socket..."
 
 s.send(json.dumps(freq_table, -1))
-# s.send(encoded)
+# sleep for some time
+for x in xrange(1,9999999):
+  pass
+
+s.send(encoded)
+
+# ======================== CLIENT OUTPUT ========================
+# 
+# The string is : I have been and always shall be your friend
+# ============ HUFFMAN CODES ============
+# CHAR FREQ CODE
+#       8   111
+# e     5   011
+# a     5   100
+# n     3   1011
+# l     3   1010
+# y     2   0100
+# r     2   0101
+# s     2   11010
+# h     2   11011
+# d     2   0001
+# b     2   0000
+# v     1   110010
+# w     1   110011
+# u     1   11000
+# i     1   00101
+# o     1   00100
+# I     1   00111
+# f     1   00110
+# Encoded string: 00111111110111001100100111110000011011101111110010110001111100101011001110001001101011111010110111001010101011100000111110100001001100001011110011001010010101110110001
+# 
+# Connecting to server at localhost on port 2345...
+# Sending Huffman tree and encoded string over socket...
--- BE/daa/huffman_coding/server.py
+++ BE/daa/huffman_coding/server.py
@@ -2,6 +2,21 @@ import socket
 import huffman
 import json
 
+def recreate_message(encoded_string, root):
+  message = ""
+  while len(encoded_string) > 0:
+    traversal = root
+    while traversal.__class__.__name__ != "LeafNode":
+      if encoded_string[0] == "0":
+        traversal = traversal.left
+      elif encoded_string[0] == "1":
+        traversal = traversal.right
+      encoded_string = encoded_string[1:]
+
+    message += traversal.char
+
+  return message
+
 #create an INET, STREAMing socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #bind the socket to a public host, and a port
@@ -12,10 +27,23 @@ serversocket.listen(5)
 
 while 1:
   (clientsocket, address) = serversocket.accept()
-  freq_list               = json.loads(clientsocket.recv(5000))
-  encoded_string          = clientsocket.recv(5000)
 
+  print "Receiving information over socket..."
+  freq_list               = json.loads(clientsocket.recv(1234))
+  encoded_string          = clientsocket.recv(1234)
+
+  print "Received encoded string : " + encoded_string
+
+  print "Decoding information..."
   tree = huffman.HuffmanTree(freq_list)
-  # print encoded
-# Receive huffman codes from the client
-# Convert to letters and display them.
+  message = recreate_message(encoded_string, tree.root)
+
+  print "Message: " + message
+
+# ======================== SERVER OUTPUT ========================
+#
+# Server binding on localhost at port 2345...
+# Receiving information over socket...
+# Received encoded string : 00111111110111001100100111110000011011101111110010110001111100101011001110001001101011111010110111001010101011100000111110100001001100001011110011001010010101110110001
+# Decoding information...
+# Message: I have been and always shall be your friend
