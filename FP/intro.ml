ddfb389fe30bc8ff8b2ad6f555e77aaa0a512623
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2017-10-06 11:25:17 +0900

    FP

--- FP/intro.ml
+++ FP/intro.ml
@@ -16,4 +16,25 @@ let rec circle_areas r_list =
     [] -> []
   | hd :: tl -> circle_area hd :: circle_areas tl;;
 
+type iorf = Int of int | Float of float | Error;;
 
+let div_num num1 num2 =
+  match (num1, num2) with
+    (Int n1, Int n2) -> Int(n1 / n2)
+  | (Int n1, Float f2) -> Float(float n1 /. f2)
+  | (Float num1, Int num2) -> Float(num1 /. float num2)
+  | (Float num1, Float num2) -> Float(num1 /. num2)
+  | (_, Error) -> Error
+  | (Error, _) -> Error;;
+                                     
+type 'a binary_tree =
+  Empty
+| Node of 'a * 'a binary_tree * 'a binary_tree;;
+
+
+let rec find x tree =
+  match tree with
+    Empty -> false
+  | Node(y, left, right) ->
+     if x = y then true else
+       if x < y then find y left else find y right;;
