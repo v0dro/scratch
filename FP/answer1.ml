37647a5215ae29edba58d6c243d6e395ba210c9c
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2017-10-07 23:10:25 +0900

    fp

--- FP/answer1.ml
+++ FP/answer1.ml
@@ -8,7 +8,7 @@ let pi = 4.0 *. atan 1.0;;
 let circle_area radius =
   pi *. radius *. radius;;
 
-let radius_of_circle area =        
+let radius_of_circle area =
   sqrt (area /. pi);;
 
 let rec circle_areas r_list =
@@ -31,21 +31,6 @@ type point_type = {mutable x: float; mutable y: float};;
 
 let rotate mp angle =
   let angle_rad = angle *. pi  /. 180. in
-  let rotation =
-    [|
-      [|cos angle_rad; -.(sin angle_rad) |];
-      [|sin angle_rad; cos angle_rad |]
-    |] in
-  mp.x <- (rotation.(0).(0) *. mp.x) +. (rotation.(0).(1) *. mp.y);
-  mp.y <- (rotation.(1).(0) *. mp.x) +. (rotation.(1).(1) *. mp.y);;
-
-type 'a binary_tree =
-  Empty
-| Node of 'a * 'a binary_tree * 'a binary_tree;;
-
-let rec find x tree =
-  match tree with
-    Empty -> false
-  | Node(y, left, right) ->
-     if x = y then true else
-       if x < y then find y left else find y right;;
+  let x = (mp.x *. cos angle_rad) -. (mp.y *. sin angle_rad) in
+  let y = (mp.y *. cos angle_rad) +. (mp.y *. sin angle_rad) in
+  mp.x <- x; mp.y <- y;;
