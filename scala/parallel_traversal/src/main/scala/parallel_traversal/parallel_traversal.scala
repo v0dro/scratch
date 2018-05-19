6c0bc279f5d44ea6f5758d4e8db2474d1c6a207b
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-05-19 13:44:22 +0900

    more stuff with scala

--- scala/parallel_traversal/src/main/scala/parallel_traversal/parallel_traversal.scala
+++ scala/parallel_traversal/src/main/scala/parallel_traversal/parallel_traversal.scala
@@ -1,14 +1,22 @@
 package parallel_traversal.parallel_traversal
 
 import neko._
+import neko.util.TraversalClient
 
 object ParallelTraversal {
-
+  case class Token(from: PID, to: PID) extends UnicastMessage
 }
 
 class ParallelTraversal(c: ProcessConfig) extends ReactiveProtocol(c, "parallel traversal")
 {
   import ParallelTraversal._
+  import TraversalClient._
+  import Tarry._
+
+  private var children = List.empty[PID]
+  private var parent : Option[PID] = None
+
+  def 
 
   def onReceive = PartialFunction.empty[Event,Unit]
 
