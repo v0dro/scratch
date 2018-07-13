83432bb379f1fba331dc0022176dd1559347dd39
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-07-13 20:05:36 +0900

    update runner

--- c_shizzle/parallel/row_major_dist_lu/run.sh
+++ c_shizzle/parallel/row_major_dist_lu/run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-rm *.txt
-make clean
-make -j8 -k sync_lu
-python remake_matrix.py $1
+if [ "$(whoami)" != "17M38101" ]; then
+    rm *.txt
+    make clean
+    make -j8 -k sync_lu
+    python remake_matrix.py $1
+else
+    echo "Running in TSUBAME debug mode..." 
+    rm *.txt
+    make clean
+    make -j8 -k sync_lu_tsubame
+    python remake_matrix.py $1   
+fi
 
