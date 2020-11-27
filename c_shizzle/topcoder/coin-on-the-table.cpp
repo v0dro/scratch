3bfad09f140fd676f6cc6694dec875f939427a0f
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2020-11-27 11:23:46 +0900

    wrote DFS algo

--- c_shizzle/topcoder/coin-on-the-table.cpp
+++ c_shizzle/topcoder/coin-on-the-table.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
 // https://www.hackerrank.com/challenges/coin-on-the-table/problem?h_r=internal-search&h_r=next-challenge&h_v=zen
 
+void search(int m, int n, int k, vector<vector<char>> grid) {
+  int i = 1, j = 1;
+}
 
 int main(int argc, char *argv[])
 {
+  int m, n, k;
+  vector<vector<char>> grid;
   
+  cin >> m >> n >> k;
+  grid.resize(m);
+  for (int i = 0; i < m; i++) {
+    grid[i].resize(n);
+    char temp;
+    for (int j = 0; j < n; j++) {
+      cin >> temp;
+      grid[i][j] = temp;
+    }
+  }
+  
+  search(m, n, k, grid);
   return 0;
 }
---
+++ c_shizzle/topcoder/dfs.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+typedef pair<int, int> ii;      // pair of (neighbour, weight)
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
+#define VISITED 1
+#define UNVISITED -1
+
+vi tracker;
+
+void dfs(int node, vector<vii>& graph) {
+  tracker[node] = VISITED;
+  cout << "n: " << node << endl;
+
+  for (int i = 0; i < graph[node].size(); i++) {
+    ii new_node = graph[node][i];
+    if (tracker[new_node.first] == UNVISITED) {
+      dfs(new_node.first, graph);
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int m = 7;
+  int w = 0;
+  vector<vii> graph(m);
+  tracker.resize(m, UNVISITED);
+
+  graph[0].push_back(ii(2, w));
+  graph[0].push_back(ii(1, w));
+
+  graph[1].push_back(ii(0, w));
+  graph[1].push_back(ii(2, w));
+  graph[1].push_back(ii(4, w));
+
+  graph[2].push_back(ii(0, w));
+  graph[2].push_back(ii(1, w));
+  graph[2].push_back(ii(5, w));
+
+  graph[4].push_back(ii(1, w));
+
+  graph[5].push_back(ii(2, w));
+  graph[5].push_back(ii(6, w));
+
+  graph[6].push_back(ii(5, w));
+
+  dfs(0, graph);
+}
