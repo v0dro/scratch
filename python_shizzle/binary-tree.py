a3a7ddd536519c9359c2c18a919fad8773ba252c
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-03-06 15:53:47 +0900

    python binary tree

---
+++ python_shizzle/binary-tree.py
@@ -0,0 +1,46 @@
+class BinaryTree:
+    def __init__(self):
+        self.data = None
+        self.left = None
+        self.right = None
+
+    def inorder(self):
+        if self.data:
+            if self.left:
+                self.left.inorder()
+            print("data: ", self.data)
+            if self.right:
+                self.right.inorder()
+
+    def preorder(self):
+        if self.data:
+            print("data: " , self.data)
+            if self.left:
+                self.left.preorder()
+            if self.right:
+                self.right.preorder()
+                
+    def insert(self, value):
+        if self.data == None:
+            self.data = value
+        else:
+            if value <= self.data:
+                if not self.left:
+                    self.left = BinaryTree()
+                self.left.insert(value)
+            elif value > self.data:
+                if not self.right:
+                    self.right = BinaryTree()
+                self.right.insert(value)
+
+t = BinaryTree()
+t.insert(4)
+t.insert(1)
+t.insert(-3)
+t.insert(10)
+t.insert(11)
+t.insert(9)
+
+t.inorder()
+print("\n")
+t.preorder()
