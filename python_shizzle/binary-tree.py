8124efdd874a08fd9e5617c8c6218e9c5c4ff0a4
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-03-10 19:35:43 +0900

    update LCA solution

--- python_shizzle/binary-tree.py
+++ python_shizzle/binary-tree.py
@@ -1,6 +1,7 @@
 class BinaryTree:
-    def __init__(self):
+    def __init__(self, _parent=None):
         self.data = None
+        self.parent = _parent
         self.left = None
         self.right = None
 
@@ -35,15 +36,39 @@ class BinaryTree:
         else:
             if value <= self.data:
                 if not self.left:
-                    self.left = BinaryTree()
+                    self.left = BinaryTree(self)
                 self.left.insert(value)
             elif value > self.data:
                 if not self.right:
-                    self.right = BinaryTree()
+                    self.right = BinaryTree(self)
                 self.right.insert(value)
 
     def first_common_ancestor(self, n1, n2):
-        pass
+        stack = [self]
+        parents = { self.data : None }
+
+        while n1 not in parents or n2 not in parents:
+            node = stack.pop()
+            # keep saving the pointers when traversing the tree
+            if node.left:
+                parents[node.left.data] = node.data
+                stack.append(node.left)
+
+            if node.right:
+                parents[node.right.data] = node.data
+                stack.append(node.right)
+
+        # ancestors set() for node p
+        ancestors = set()
+
+        while n1:
+            ancestors.add(n1)
+            n1 = parents[n1]
+
+        while n2 not in ancestors:
+            n2 = parents[n2]
+
+        return n2
 
     def is_descendant(self, parent, child):
         is_in_child = self.postorder_search(child)
@@ -59,3 +84,6 @@ t.insert(9)
 t.inorder()
 print("\n")
 t.preorder()
+
+ee = t.first_common_ancestor(-3, 11)
+print(ee)
