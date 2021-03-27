50cf7dd753c3df2c20e0078f3fa52dbfc87dae07
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-03-27 10:12:40 +0900

    update abbrev example

---
+++ python_shizzle/abbrevation.py
@@ -0,0 +1,30 @@
+#https://www.hackerrank.com/challenges/abbr/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming
+
+# Simple solution (no dyn prog)
+# Failing case:
+
+# KXzQ
+# K
+
+# Reason: problem states that you can delete only lowercase
+# letters from a.
+def abbrev(a, b):
+    i = 0
+    j = 0
+    while i < len(a) and j < len(b):
+        print("a(i)=", a[i],  " b(j)=", b[j])
+        if a[i] == b[j] or a[i].upper() == b[j]:
+            i += 1
+            j += 1
+            continue
+        
+        if a[i] != b[j] or a[i].upper() != b[j]:
+            i += 1
+            continue
+            
+    if j == len(b):
+        return "YES"
+    else:
+        return "NO"
+
+print(abbrev("KXzQ", "K"))
