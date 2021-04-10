df02f1ae19c4c020f8ee48bd28da8c110cadd2a6
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-04-11 04:21:00 +0900

    update python loops

--- python_shizzle/lr_check.py
+++ python_shizzle/lr_check.py
@@ -24,6 +24,13 @@ for nc in range(0, rank, 4):
             print( "\t\tExn -> xc: ", xc, " nc: ", nc, "Bx -> ", " nc: ", nc ," yc: ", yc)
 
 
+for mc in range(0, rank, 4):
+    for nc in range(0, rank, 4):
+        print("Cmn -> mc: ", mc, " nc: ", nc)
+        for xc in range(0, rank, 4):
+            print("\tAx -> xc: ", xc, " mc: ", mc, " Cmn -> mc: ", mc, " nc: ", nc)
+            print("\t\tExn -> xc: ", xc, " nc: ", nc, " Bx -> nc: ", nc, " yc: ", nc)
+            
 # Cmn -> mc:  0  nc:  0
 # 	 Ax  -> xc:  0  mc:  0  nc:  0
 # 	 Exn -> xc:  0  nc:  0  yc:  0
