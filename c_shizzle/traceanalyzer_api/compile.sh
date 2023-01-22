44ff0441be14ee98b16aaee5545d6dfc511ad0bb
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2023-01-22 18:34:33 +0900

    update main

--- c_shizzle/traceanalyzer_api/compile.sh
+++ c_shizzle/traceanalyzer_api/compile.sh
@@ -7,4 +7,8 @@ module load intel-mpi/2021.5 gcc intel-mkl/2022.0.0 cmake/3.22.3 intel-vtune int
 
 source /apps/intel/2022.1/itac/2021.5.0/env/vars.sh
 
-mpicxx main.cpp
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$VT_ROOT/slib
+
+mpicxx -I$VT_ROOT/include main.cpp -L$VT_ROOT/slib -lVT -o main
+
+mpirun -n 2 ./main
--- c_shizzle/traceanalyzer_api/main.cpp
+++ c_shizzle/traceanalyzer_api/main.cpp
@@ -1,3 +1,5 @@
+#include <cstdio>
+
 #include "mpi.h"
 #include "VT.h"
 
@@ -8,7 +10,32 @@ int main() {
   MPI_Init(NULL, NULL);
   MPI_Comm_size(MPI_COMM_WORLD, &MPISIZE);
   MPI_Comm_rank(MPI_COMM_WORLD, &MPIRANK);
-  MPI_Dims_create(MPISIZE, 2, MPIGRID);
+
+  // VT_traceoff();
+
+  int number;
+  if (MPIRANK == 0) {
+    number = -1;
+    MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+  } else if (MPIRANK == 1) {
+    MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+    printf("Process 1 received number %d from process 0\n",
+           number);
+  }
+
+  // VT_traceon();
+
+
+  if (MPIRANK == 0) {
+    number = -1;
+    MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+  } else if (MPIRANK == 1) {
+    MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+    printf("Process 1 received number %d from process 0\n",
+           number);
+  }
 
   MPI_Finalize();
 
